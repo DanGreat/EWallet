1f5cd458f40ad8a254043eed0c38872b6b8ba29e
DanGreat <dbassey360@gmail.com>
2021-01-22 18:19:05 +0100

    E-wallet design with ionic 5 and angular 9

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { AnimationController, Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
-  constructor() {}
+  @ViewChild('wallet', {read: ElementRef, static: true}) wallet: ElementRef;
+  showHeaderBorder = true;
+
+  constructor(private animationCtrl: AnimationController) {}
+
+  ngOnInit(){
+    const animation: Animation = this.animationCtrl.create()
+    .addElement(this.wallet.nativeElement)
+    .duration(1500)
+    .fromTo('opacity', '0', '1')
+    .fromTo('transform', 'scale(.2)', 'scale(1)');
+
+    animation.play();
+  }
+
+  didScroll(ev){
+    const top = ev.detail.scrollTop;
+    if (top > 10){
+      this.showHeaderBorder = false;
+    }else{
+      this.showHeaderBorder = true;
+    }
+  }
 
 }
